3e94175be7e5016f617b45c6899c88b4c670a28e
Manon NAEL <manon.nael@audencia.com>
2023-02-21 12:30:29 +0100

    ex2

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/tasks', to: 'tasks#index'
+  get '/tasks/new', to: 'tasks#new'
+  post '/tasks', to: 'tasks#create'
+  patch '/tasks/:id', to: 'tasks#update'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :tasks_edit
+  delete '/tasks/:id', to: 'tasks#destroy'
+  get '/tasks/:id', to: 'tasks#show', as: :task
 end
